78cfe2d343ea8417aa53b147f8e3a10fdaa6fc63
Tommy <tommycrabtree@outlook.com>
2024-05-31 19:28:33 -0400

    End of Section 2

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
